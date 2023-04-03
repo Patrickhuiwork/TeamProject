3ebe288f086be07f6a50ce7422a3aa8466aa3955
Patrickhuiwork <112998836+Patrickhuiwork@users.noreply.github.com>
2023-04-02 20:35:27 -0700

    navbar

---
+++ components/navbarcomponent/NavItem.jsx
@@ -0,0 +1,13 @@
+
+import Link from 'next/link';
+import React from 'react';
+
+const NavItem = ({ title, href, active }) => {
+  return (
+    <Link href={href} className={`nav__item ${active ? 'active' : ''}`}>
+      {title}
+    </Link>
+  );
+};
+
+export default NavItem;
---
+++ components/navbarcomponent/Navbar.jsx
@@ -0,0 +1,68 @@
+import Link from 'next/link'
+import React, { useState } from 'react'
+import NavItem from './NavItem'
+const MENU_LIST = [
+    {
+        title: "Quizzes",
+        href:"/"
+    },
+    {
+        title: "Learn More",
+        href:"/"
+    },
+    {
+        title: "About Us",
+        href:"/"
+    },
+    {
+        title: "Tutorial",
+        href:"/"
+    },
+    {
+        title: "Resources",
+        href:"/"
+    }
+]
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
+  return (
+      <header>
+          <nav className='nav'>
+                <Link href={"/"} legacyBehavior>
+                <a>
+                    <img src="/public/Logo/horizontal logo v2.svg" alt="heathelp" className="logo" />
+                </a>
+                </Link>
+
+              <div className="nav__menu-bar" onClick={toggleMenu}>
+                  <div className={`nav__menu-bar-line ${isOpen ? 'open' : ''}`}></div>
+                  <div className={`nav__menu-bar-line ${isOpen ? 'open' : ''}`}></div>
+                  <div className={`nav__menu-bar-line ${isOpen ? 'open' : ''}`}></div>
+              </div>
+
+              <div className={`nav__menu-list ${isOpen ? 'open' : ''}`}>
+                  {
+                      MENU_LIST.map((menu, idx) => {
+                          return <div key={menu.text}>
+                              <NavItem {...menu} onClick={closeMenu} />
+                          </div>
+                      })
+                  }
+
+              </div>
+          </nav>
+      </header>
+  );
+}
+
+export default Navbar
