3b8df8a939518d456d9fdcc30a9657590af8c487
Patrickhuiwork <112998836+Patrickhuiwork@users.noreply.github.com>
2023-04-10 14:09:50 -0700

    asd

--- pages/activities.js
+++ pages/activities.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '@/styles/page2.module.css'
 
 export default function Activites() {
   return (
