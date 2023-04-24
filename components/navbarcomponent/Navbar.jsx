89d052b1e90a21914695f13ad117d83ea83edf36
Patrickhuiwork <112998836+Patrickhuiwork@users.noreply.github.com>
2023-04-23 21:55:55 -0700

    modified nav bar
    still needs more work closes when outside is clicked now but doesnt close on link click and when you click on the x its buggy

--- components/navbarcomponent/Navbar.jsx
+++ components/navbarcomponent/Navbar.jsx
@@ -1,36 +1,38 @@
-import Link from 'next/link'
-import React, { useState } from 'react'
-import Image from 'next/image'
-import NavItem from './NavItem'
+import Link from 'next/link';
+import React, { useState, useEffect, useRef } from 'react';
+import Image from 'next/image';
+import NavItem from './NavItem';
+
 const MENU_LIST = [
-    {
-        title: "Home",
-        href:"/getstarted"
-    },
-    {
-        title: "Quizzes",
-        href:"/Quizzes"
-    },
-    {
-        title: "Learn More",
-        href:'/learning'
-    },
-    {
-        title: "About Us",
-        href:"/about"
-    },
-    {
-        title: "Tutorial",
-        href:"/tutorial"
-    },
-    {
-        title: "Resources",
-        href:"/resources"
-    }
-]
+  {
+    title: 'Home',
+    href: '/getstarted',
+  },
+  {
+    title: 'Quizzes',
+    href: '/Quizzes',
+  },
+  {
+    title: 'Learn More',
+    href: '/learning',
+  },
+  {
+    title: 'About Us',
+    href: '/about',
+  },
+  {
+    title: 'Tutorial',
+    href: '/tutorial',
+  },
+  {
+    title: 'Resources',
+    href: '/resources',
+  },
+];
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef();
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
@@ -40,32 +42,49 @@ const Navbar = () => {
     setIsOpen(false);
   };
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (isOpen && !menuRef.current.contains(event.target)) {
+        closeMenu();
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isOpen]);
+
   return (
-      <header>
-          <nav className='nav'>
-                <Link href={"/getstarted"} legacyBehavior>
-                    <Image src="/Logo/horizontal logo v2.svg" width="200" height="50" alt="heathelp" className="logo" />
-                </Link>
+    <header>
+      <nav className='nav'>
+        <Link href={'/getstarted'} legacyBehavior>
+          <Image
+            src='/Logo/horizontal logo v2.svg'
+            width='200'
+            height='50'
+            alt='heathelp'
+            className='logo'
+          />
+        </Link>
 
-              <div className="nav__menu-bar" onClick={toggleMenu}>
-                  <div className={`nav__menu-bar-line ${isOpen ? 'open' : ''}`}></div>
-                  <div className={`nav__menu-bar-line ${isOpen ? 'open' : ''}`}></div>
-                  <div className={`nav__menu-bar-line ${isOpen ? 'open' : ''}`}></div>
+        <div className='nav__menu-bar' onClick={toggleMenu}>
+          <div className={`nav__menu-bar-line ${isOpen ? 'open' : ''}`}></div>
+          <div className={`nav__menu-bar-line ${isOpen ? 'open' : ''}`}></div>
+          <div className={`nav__menu-bar-line ${isOpen ? 'open' : ''}`}></div>
+        </div>
+
+        <div ref={menuRef} className={`nav__menu-list ${isOpen ? 'open' : ''}`}>
+          {MENU_LIST.map((menu, idx) => {
+            return (
+              <div key={menu.text}>
+                <NavItem {...menu} onClick={closeMenu} />
               </div>
-
-              <div className={`nav__menu-list ${isOpen ? 'open' : ''}`}>
-                  {
-                      MENU_LIST.map((menu, idx) => {
-                          return <div key={menu.text}>
-                              <NavItem {...menu} onClick={closeMenu} />
-                          </div>
-                      })
-                  }
-
-              </div>
-          </nav>
-      </header>
+            );
+          })}
+        </div>
+      </nav>
+    </header>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
