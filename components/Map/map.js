1470053c2ae6ff1741650b943b76e828548b7955
Patrickhuiwork <112998836+Patrickhuiwork@users.noreply.github.com>
2023-05-12 22:12:53 -0700

    added all the firehalls in vancouver
    added all the firehalls pls style the rest of the pages further they are not complete yet

--- components/Map/map.js
+++ components/Map/map.js
@@ -14,7 +14,27 @@ export default function Map() {
         [49.26288237291829, -123.13764964504067],
         [49.22013610708225, -123.04173430455938],
         [49.283620951637126, -123.12583337202867],
+        [49.26857098520486, -123.06621891805166],
+        [49.258942881822755, -123.23799447018341],
+        [49.264898292592775, -123.17300699774235],
+        [49.278068521794985, -123.04660258707302],
+        [49.252849702041736, -123.04076612608142],
+        [49.2199233628724, -123.07683120271315],
+        [49.237470146792326, -123.13663964528442],
+        [49.26259933755884, -123.20592201579599],
+        [49.23650887754219, -123.06584822124628],
+        [49.23754719452154, -123.17325620086592],
+        [49.21748269528255, -123.12953098737236]
+
+
         
+
+
+
+
+
+
+
 
 
     ];
@@ -37,11 +57,17 @@ export default function Map() {
                         {index === 5 && "Vancouver Fire Hall No. 5"}
                         {index === 6 && "Vancouver Fire Hall No. 6 | West End"}
                         {index === 7 && "Vancouver Fire Hall No. 7 | Downtown"}
-                        {index === 2 && "Vancouver Fire Hall No. 8 | Yaletown"}
-                        {index === 2 && "Vancouver Fire Hall No. 8 | Yaletown"}
-                        {index === 2 && "Vancouver Fire Hall No. 8 | Yaletown"}
-                        {index === 2 && "Vancouver Fire Hall No. 8 | Yaletown"}
-                        {index === 2 && "Vancouver Fire Hall No. 8 | Yaletown"}
+                        {index === 8 && "Vancouver Firehall No. 9"}
+                        {index === 9 && "Vancouver Firehall No. 10 | University"}
+                        {index === 10 && "Vancouver Fire Hall No. 12 | Kitsilano"}
+                        {index === 11 && "Vancouver Fire Hall No. 14 | Hastings Sunrise"}
+                        {index === 12 && "Vancouver Fire Hall No. 15 | Renfrew"}
+                        {index === 13 && "Vancouver Fire Hall No. 17 | Fraserview"}
+                        {index === 14 && "Vancouver Fire Hall No. 18 | Shaugnessy"}
+                        {index === 15 && "Vancouver Fire Hall No. 19"}
+                        {index === 16 && "Vancouver Fire Hall No. 20 | Victoria"}
+                        {index === 12 && "Vancouver Fire Hall No. 15 | Vancouver Fire Hall No. 21 - Kerrisdale"}
+                        {index === 12 && "Vancouver Fire Hall No. 22 - Marpole"}
                     </Popup>
                 </Marker>
             ))}
